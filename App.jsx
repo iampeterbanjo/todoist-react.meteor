6eb4b0af9650fede9e2e4a1a34a0a5b8fdbd78b4
iampeterbanjo <peterbanjo@gmail.com>
2016-01-19 12:53:32 +0000

    Filter completed tasks

--- App.jsx
+++ App.jsx
@@ -1,10 +1,29 @@
 App = React.createClass({
 	// This mixin makes the getMeteorData method work
 	mixins: [ReactMeteorData],
+	
+	getInitialState() {
+		return {
+			hideCompleted: false
+		}
+	},
+	
+	toggleHideCompleted() {
+		this.setState({
+			hideCompleted: !this.state.hideCompleted
+		});
+	},
 
 	getMeteorData() {
+		let query = {};
+		
+		if (this.state.hideCompleted) {
+			query = {checked: {$ne: true}};
+		}
+
 		return {
-			tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+			tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+			incompleteCount: Tasks.find({checked: {$ne: true}}).count()
 		};
 	},
 
@@ -31,7 +50,16 @@ App = React.createClass({
 		return (
 			<div className="container">
 				<header>
-					<h1>Todo List</h1>
+					<h1>Todo List ({this.data.incompleteCount})</h1>
+					
+					<label className="hide-completed">
+						<input
+							readOnly={true}
+							checked={this.state.hideCompleted}
+							onClick={this.toggleHideCompleted}
+							type="checkbox" />
+						Hide Completed Tasks
+					</label>
 
 					<form className="new-task" onSubmit={this.handleSubmit} >
 						<input type="text" ref="textInput" placeholder="Type to add new tasks" />
